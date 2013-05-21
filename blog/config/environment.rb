7d75e95588a4b706b6eb70fc0b11c975d97e486e
wwb0709 <wwb1111@163.com>
2013-05-21 22:42:13 +0800

    a

--- blog/config/environment.rb
+++ blog/config/environment.rb
@@ -1,10 +1,8 @@
 # Load the rails application
+# encoding: utf-8 
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Blog::Application.initialize!
 
 require 'will_paginate'
-
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = '« 上一页'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = '下一页 »'
